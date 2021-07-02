1512de34198c4bb7ab2e471543d8dd2ea32be43c
calvix <vaclav@giantswarm.io>
2021-07-02 13:15:02 +0200

    add-aws-client-getter

---
+++ pkg/iam/error.go
@@ -0,0 +1,24 @@
+package iam
+
+import (
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	awsiam "github.com/aws/aws-sdk-go/service/iam"
+)
+
+func IsNotFound(err error) bool {
+	if aerr, ok := err.(awserr.Error); ok {
+		if aerr.Code() == awsiam.ErrCodeNoSuchEntityException {
+			return true
+		}
+	}
+	return false
+}
+
+func IsAlreadyExists(err error) bool {
+	if aerr, ok := err.(awserr.Error); ok {
+		if aerr.Code() == awsiam.ErrCodeEntityAlreadyExistsException {
+			return true
+		}
+	}
+	return false
+}
