8fd288b46ae997c04431064eb11e27af5ff4b03b
Marcus Noble <AverageMarcus@users.noreply.github.com>
2022-11-24 12:00:56 +0000

    Check for other uses of the same IAM role before deleting (#131)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+
+- Check for other resources using the same IAM instance profile as the resource being deleted and skip deleting the IAM role if others found.
+
 ## [0.4.2] - 2022-11-01
 
 ### Changed
---
+++ controllers/capa_utils.go
@@ -0,0 +1,43 @@
+package controllers
+
+import (
+	"context"
+
+	capa "sigs.k8s.io/cluster-api-provider-aws/api/v1alpha3"
+	expcapa "sigs.k8s.io/cluster-api-provider-aws/exp/api/v1alpha3"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func isRoleUsedElsewhere(ctx context.Context, ctrlClient client.Client, roleName string) (bool, error) {
+	var err error
+
+	var awsMachineTemplates capa.AWSMachineTemplateList
+	err = ctrlClient.List(
+		ctx,
+		&awsMachineTemplates,
+	)
+	if err != nil {
+		return false, err
+	}
+	for _, mt := range awsMachineTemplates.Items {
+		if mt.DeletionTimestamp != nil && mt.Spec.Template.Spec.IAMInstanceProfile == roleName {
+			return true, err
+		}
+	}
+
+	var awsMachinePools expcapa.AWSMachinePoolList
+	err = ctrlClient.List(
+		ctx,
+		&awsMachinePools,
+	)
+	if err != nil {
+		return false, err
+	}
+	for _, mt := range awsMachinePools.Items {
+		if mt.DeletionTimestamp != nil && mt.Spec.AWSLaunchTemplate.IamInstanceProfile == roleName {
+			return true, err
+		}
+	}
+
+	return false, err
+}
